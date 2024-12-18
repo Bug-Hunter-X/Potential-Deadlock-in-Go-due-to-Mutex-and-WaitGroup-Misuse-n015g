13043320f5807cbfa3d02bcd19fe2134b0b9a684
BugHunter-X <askzenai@gmail.com>
2024-12-18 23:38:05 +0100

    Add bugSolution.go file

---
+++ bugSolution.go
@@ -0,0 +1,15 @@
+```go
+func main() {
+    var m sync.Mutex
+    wg := sync.WaitGroup{}
+    wg.Add(1)
+    go func() {
+        defer wg.Done()
+        m.Lock()
+        fmt.Println("Goroutine 1 locked the mutex")
+        time.Sleep(2 * time.Second)
+        m.Unlock()
+    }()
+    wg.Wait()
+    fmt.Println("Main goroutine finished")
+}```
